4257dbe0cab6f66757dcb2ad508347886aa83354
Kat <admifruk@gmail.com>
2022-07-02 09:54:32 +0200

    patterns

--- view/createuser.php
+++ view/createuser.php
@@ -10,16 +10,14 @@ include './view/menu.php';
         <form action="" method="POST">
             
             <label for="name_user">Prénom</label>
-            <input type="text" name="name_user" pattern="(?=.[A-Za-z]).{2,}">
+            <input type="text" name="name_user" pattern="(?=.[A-Za-z]).{1,}">
     
             <label for="surname_user">Nom de famille</label>
-            <input type="text" name="surname_user">
+            <input type="text" name="surname_user" pattern="(?=.[A-Za-z]).{1,}">
             
             <label for="email_user">Email</label>
             <input type="email" name="email_user" pattern="[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,}$">
-            <!-- here the question mark matches with the string but, not the whitespace regex the *\d dictates all characters and numbers
-            This pattern requires the password to be at least 12 characters long and must contain at least one uppercase and one lowercase letter and a number
-         -->
+           
             <label for="password_user">Choisir votre mot de passe</label>
             <input  class="viewPass" class="hidePass" type="password" name="password_user" pattern ="(?=.*\d)(?=.*[a-z0-9-])(?=.*[A-Z]).{12,}">
             
